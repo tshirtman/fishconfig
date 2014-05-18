de2632be9c675b067987dfda8a1ec517d29f9b8a
tshirtman <gabriel.pettier@gmail.com>
2014-05-18 11:59:29 +0200

    ignore fish_history and add init.sh

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+while true
+do
+read -p "do you want to link $PWD to ~/.config/fish (Y/N)" yn
+case $yn in
+	[Yy]*)
+		rm -rf ~/.config/fish;
+		ln -sf $PWD ~/.config/fish;
+		break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+
+while true
+do
+read -p "do you want to install fish (Y/N)" yn
+case $yn in
+	[Yy]*) sudo apt-get install fish;
+		chsh -s /usr/bin/fish;
+		break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
